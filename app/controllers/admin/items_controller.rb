03aec688acd454742d8897af4740eedec8ad10a6
EC2 Default User <ec2-user@ip-172-31-35-213.ec2.internal>
2022-02-12 14:08:46 +0000

    0212_23:00_一覧

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,17 +1,30 @@
 class Admin::ItemsController < ApplicationController
+
   def new
-  @image = Item.new
+  @item = Item.new
   end
 
   def create
+   @item = Item.new(item_params)
+   @item.save
+    redirect_to new_admin_item_path
   end
 
   def index
+   @items = Item.all
+   
   end
 
   def show
+   @item = Item.find(params[:id])
   end
 
   def destroy
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre, :price)
+  end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '44feef785702e9b5d6cb78b380fc4c1d315fe5afd84175c2bde1d991075cf6af8856dab8bf6302b375f34411e142d1114831f3bc43ea40fbd4cb845afb3c6997'
