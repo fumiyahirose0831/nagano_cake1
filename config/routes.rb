a8fc398d4904e28101c003ed7a37d65bb2be3931
EC2 Default User <ec2-user@ip-172-31-35-213.ec2.internal>
2022-02-15 05:32:31 +0000

    admin_会員一覧修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
+   devise_for :customers, controllers: {
+    sessions:      'public/sessions',
+    passwords:     'public/passwords',
+    registrations: 'public/registrations'
+  }
+  
+  
+   devise_for :admins, controllers: {
+    sessions:      'admin/sessions',
+    passwords:     'admin/passwords',
+    registrations: 'admin/registrations'
+  }
 
   root to: 'homes#top'
 
