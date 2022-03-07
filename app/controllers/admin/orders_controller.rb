2553c777c176444c7cbd8a948673ba5b55ac1bf2
EC2 Default User <ec2-user@ip-172-31-35-213.ec2.internal>
2022-03-07 11:10:34 +0000

    3/7_20:09

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,7 @@
 class Admin::OrdersController < ApplicationController
+
 def show
-@order = Order.find(orders[:id])
+ @order =  Order.find(params[:id])
 end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,12 +5,15 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
   def active_for_authentication?
     super && (is_deleted == false)
-    
-
-
   end
+
     has_many :cart_items, dependent: :destroy
     has_many :orders, dependent: :destroy
-  
-  
+
+
+  def full_name
+    self.last_name.to_s + " " + self.first_name.to_s
+  end
+
+
 end
